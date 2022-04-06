46a9c99cb92690d67b15d021471cfaf23f82cb80
krbz999 <50169243+krbz999@users.noreply.github.com>
2022-04-06 18:11:32 +0200

    Update const.mjs

--- scripts/const.mjs
+++ scripts/const.mjs
@@ -1,3 +1,4 @@
 export const MODULE_NAME = "dicerecharge";
-export const MODULE_TITLE = "Zhell's Formulaic Recharging";
+export const MODULE_TITLE = "Z's Formulaic Recharging";
 export const MODULE_TITLE_SHORT = "ZHELL";
+export const MODULE_FORMULA = "recovery-formula";
