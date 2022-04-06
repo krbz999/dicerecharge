6fef14d14d43ebd865f948a098d2107a82e59fcf
krbz999 <50169243+krbz999@users.noreply.github.com>
2022-04-06 18:11:02 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
-import { MODULE_TITLE, MODULE_TITLE_SHORT } from "./scripts/const.mjs";
+import { MODULE_TITLE, MODULE_NAME } from "./scripts/const.mjs";
 import { registerSettings } from "./scripts/settings.mjs";
 
 Hooks.on("init", () => {
-    console.log(`${MODULE_TITLE_SHORT} | Initializing ${MODULE_TITLE}`);
+    console.log(`${MODULE_NAME} | Initializing ${MODULE_TITLE}`);
     registerSettings();
 });
