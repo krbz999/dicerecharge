6e5fd62d51e7e4378da59c2295694da2ed98ffc3
krbz999 <50169243+krbz999@users.noreply.github.com>
2022-04-06 18:11:48 +0200

    Update custom-recharge.mjs

--- scripts/custom-recharge.mjs
+++ scripts/custom-recharge.mjs
@@ -1,59 +1,79 @@
 import { MODULE_NAME } from "./const.mjs";
+import { MODULE_FORMULA } from "./const.mjs";
 import { SETTING_NAMES } from "./settings.mjs";
 
-// Add 'dawn' and 'dusk' recharge methods
+/* Add 'dawn' and 'dusk' recharge methods. */
 Hooks.on("ready", () => {
 	if(!game.settings.get(MODULE_NAME, SETTING_NAMES.FORMULA_RECHARGE)) return;
-	CONFIG.DND5E.limitedUsePeriods.dawn = 'Dawn';
-	CONFIG.DND5E.limitedUsePeriods.dusk = 'Dusk';
+	CONFIG.DND5E.limitedUsePeriods.dawn = "Dawn";
+	CONFIG.DND5E.limitedUsePeriods.dusk = "Dusk";
 });
 
-// Add a charge recovery field
+/* Add a charge recovery field. */
 Hooks.on("renderItemSheet5e", (itemSheet, html, _) => {
 	if(!game.settings.get(MODULE_NAME, SETTING_NAMES.FORMULA_RECHARGE)) return;
-	if(!['dawn', 'dusk'].includes(itemSheet.item.data.data.uses?.per)) return;
+	if(!["dawn", "dusk"].includes(itemSheet.item?.getChatData().uses?.per)) return;
+	
+	const recoveryFormula = itemSheet.item.getFlag(MODULE_NAME, MODULE_FORMULA);
 	
 	const div = document.createElement('div');
 	div.setAttribute('class', 'form-group recharge-formula');
 	div.innerHTML = `
-		<label>Charge recovery formula</label>
+		<label>Recovery formula</label>
 		<div class="form-fields">
-			<input type="text" name="flags.zhell-recharge.recovery-formula" value="${itemSheet.document.data.flags["zhell-recharge"]?.["recovery-formula"] ?? ''}">
+			<input type="text" name="flags.${MODULE_NAME}.${MODULE_FORMULA}" value="${recoveryFormula}">
 		</div>`;
 	
 	let per = html[0].querySelector(".form-group.uses-per");
 	per.parentNode.insertBefore(div, per.nextSibling);
 });
 
-// recharge magic items on New Day, using data.flags["zhell-recharge"]["recovery-formula"]
+/* Recharge items on rest. */
 Hooks.on("restCompleted", async (actor, data) => {
 	if(!game.settings.get(MODULE_NAME, SETTING_NAMES.FORMULA_RECHARGE)) return;
 	
 	if(!data.newDay || !actor) return;
-	let rechargingItems = actor.items.filter(i => i.data?.data?.activation?.type
-		&& i.data.flags["zhell-recharge"]?.["recovery-formula"]
-		&& Roll.validate(i.data.flags["zhell-recharge"]["recovery-formula"])
-		&& i.data.data.uses.value < i.data.data.uses.max
-		&& ['dawn', 'dusk'].includes(i.data.data.uses.per));
+	const rechargingItems = actor.items.filter(i => i.getChatData().activation?.type
+		&& i.getFlag(MODULE_NAME, MODULE_FORMULA)
+		&& Roll.validate(i.getFlag(MODULE_NAME, MODULE_FORMULA))
+		&& i.getChatData().uses.value < i.getChatData().uses.max
+		&& ["dawn", "dusk"].includes(i.getChatData().uses.per));
 	
 	if(!rechargingItems.length) return;
 	
 	const flavor = `${actor.name}'s magic items recharge:`;
 	let content = `<table style="width: 100%; border: none"><thead><tr><th style="width: 60%; text-align: center">Magic Item</th><th style="width: 20%; text-align: center">Old</th><th style="width: 20%; text-align: center">New</th></tr></thead><tbody>`;
-	const speaker = {alias: 'Magic Items'}
-	const updates = rechargingItems.map(i => {
-		let uses = i.data.data.uses;
-		let rec = i.data.flags["zhell-recharge"]["recovery-formula"] ?? '0';
-		rec = Roll.replaceFormulaData(rec, actor.getRollData());
-		let iRoll = new Roll(rec).evaluate({async: false});
-		game.dice3d?.showForRoll(iRoll, game.user, true);
-		content += `<tr><td>${i.name}</td><td style="text-align: center">${uses.value}</td><td style="text-align: center">${Math.min(uses.value + iRoll.total, uses.max)}</td></tr>`;
-		return {_id: i.id, 'data.uses.value': Math.min(uses.max, uses.value + iRoll.total)};
-	});
+	const speaker = {alias: "Magic Items"};
+	const updates = [];
+	for(let item of rechargingItems){
+		const uses = item.getChatData().uses;
+		let recoveryFormula = item.getFlag(MODULE_NAME, MODULE_FORMULA) ?? "0";
+		recoveryFormula = Roll.replaceFormulaData(recoveryFormula, actor.getRollData());
+		const recoveryRoll = await new Roll(recoveryFormula).evaluate({async: true});
+		game.dice3d?.showForRoll(recoveryRoll, game.user, true);
+		content += `<tr><td>${item.name}</td><td style="text-align: center">${uses.value}</td><td style="text-align: center">${Math.min(uses.value + recoveryRoll.total, uses.max)}</td></tr>`;
+		updates.push({_id: item.id, "data.uses.value": Math.min(uses.max, uses.value + recoveryRoll.total)});
+	}
 
 	content += `</tbody></table>`;
 
-	await actor.updateEmbeddedDocuments('Item', updates);
+	await actor.updateEmbeddedDocuments("Item", updates);
 	const chatData = {flavor, content, speaker};
 	await ChatMessage.create(chatData);
+});
+
+/* Temporary migration. */
+Hooks.on("renderActorSheet5e", async (sheet, html, _) => {
+	const actor = sheet.actor;
+	const items = actor.items;
+	for(let item of items){
+		if(item.data.flags["zhell-recharge"] && item.data.flags["zhell-recharge"]["recovery-formula"]){
+			let formula = item.data.flags["zhell-recharge"]["recovery-formula"];
+			if(!item.getFlag(MODULE_NAME, MODULE_FORMULA)){
+				await item.update({"flags.-=zhell-recharge": null});
+				await item.setFlag(MODULE_NAME, MODULE_FORMULA, formula);
+				console.log(`dicerecharge | Migrated ${item.name} on actor ${actor.name}.`);
+			}
+		}
+	}
 });
