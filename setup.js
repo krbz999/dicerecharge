541be0833014aa7b49b7f0528e8817bfccb7f8bb
krbz999 <50169243+krbz999@users.noreply.github.com>
2022-05-03 23:00:17 +0200

    Add files via upload

--- setup.js
+++ setup.js
@@ -1,7 +1,10 @@
 import { MODULE_TITLE, MODULE_NAME } from "./scripts/const.mjs";
 import { registerSettings } from "./scripts/settings.mjs";
+import { api } from "./scripts/api.mjs";
 
 Hooks.on("init", () => {
     console.log(`${MODULE_NAME} | Initializing ${MODULE_TITLE}`);
     registerSettings();
+	
+	api.register();
 });
